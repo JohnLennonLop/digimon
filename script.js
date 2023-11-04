4b1c9f523183509455bc4c5ac4b325daa88a91e5
John Lennon Alves <83427706+JohnLennonLop@users.noreply.github.com>
2023-11-04 11:43:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const digimonName =document.querySelector('.digimon_name');
+// Seleciona os elementos HTML que você vai usar
+const digimonNameInput = document.getElementById('digimonName');
+const digimonForm = document.getElementById('digimonForm');
+const digimonNameElement = document.querySelector('.digimon_name');
+const digimonLevelElement = document.querySelector('.digimon_level');
+const digimonImage = document.querySelector('.digimon_image');
 
-
-const fetchDigimon = async (digimon) =>{
+const fetchDigimon = async (digimon) => {
+  try {
     const APIResponse = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${digimon}`);
-    const data= await APIResponse.json();
-    console.log(data);
+    if (!APIResponse.ok) {
+      throw new Error('Erro na solicitação da API');
+    }
+    const data = await APIResponse.json();
     return data;
-}
+  } catch (error) {
+    console.error(error);
+    console.log("Não foi possivel localizar")  }
+};
 
-const renderDigimon = async (digimon) =>{
-    const data = await fetchDigimon(digimon);
- 
-    digimonName.innerHTML = data.name;
-   
-}
-renderDigimon('agumon');
+const renderDigimon = async (digimon) => {
+  const data = await fetchDigimon(digimon);
+
+  digimonImage.src = data[0].img;
+
+  // Atualiza o nome e o nível do Digimon (se desejar)
+  //digimonNameElement.textContent = `Nome do Digimon: ${data.name}`;
+  //digimonLevelElement.textContent = `Nível do Digimon: ${data.level}`;
+};
+
+digimonForm.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Evita o envio padrão do formulário
+
+  const digimonName = digimonNameInput.value;
+  await renderDigimon(digimonName);
+});
